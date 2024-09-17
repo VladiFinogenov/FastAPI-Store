fc24ad7eea3dc2d5033197b92da0780a6fbe61bc
vlad <Finogenov.V.A@yandex.ru>
2024-09-17 23:44:48 +0300

    feat: реализация эндпоинтов для CRUD товара

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,16 +1,40 @@
-from fastapi import APIRouter
+from itertools import product
+
+from sqlalchemy import select, update
+from typing import Annotated
+
+from sqlalchemy import insert
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, status, HTTPException
+from app.backend.db_depends import get_db
+from app.schemas.schemas import CreateProduct
+from app.utils.slug import create_slug
+from app.models import Product, Category
 
 router = APIRouter(prefix='/products', tags=['products'])
 
 
 @router.get('/')
-async def all_products():
-    pass
+async def all_products(db: Annotated[Session, Depends(get_db)]):
+    products = db.scalars(select(Product).where(Product.is_active == True)).all()
+    return products
 
 
 @router.post('/create')
-async def create_product():
-    pass
+async def create_product(db: Annotated[Session, Depends(get_db)], create_product: CreateProduct):
+    db.execute(insert(Product).values(name=create_product.name,
+                                      slug=create_slug(create_product.name),
+                                      description=create_product.description,
+                                      price=create_product.price,
+                                      image_url=create_product.image_url,
+                                      stock=create_product.stock,
+                                      category_id= create_product.category,
+                                      rating=0.0))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
 
 
 @router.get('/{category_slug}')
@@ -19,15 +43,60 @@ async def product_by_category(category_slug: str):
 
 
 @router.get('/detail/{product_slug}')
-async def product_detail(product_slug: str):
-    pass
+async def product_detail(db: Annotated[Session, Depends(get_db)], product_slug: str):
+    products = db.scalars(select(Product).where(Product.slug == product_slug))
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There are no product'
+        )
+
+    return products
 
 
 @router.put('/detail/{product_slug}')
-async def update_product(product_slug: str):
-    pass
+async def update_product(db: Annotated[Session, Depends(get_db)], product_slug: str, update_product: CreateProduct):
+    product = db.scalar(select(Product).where(Product.slug == product_slug))
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no product found'
+        )
+    # Проверяем, существует ли категория
+    category_exists = db.scalar(select(Category).where(Category.id == update_product.category))
+    if not category_exists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Category not found'
+        )
+
+    db.execute(update(Product).where(Product.slug == product_slug).values(
+            name=update_product.name,
+            slug=create_slug(update_product.name),
+            description=update_product.description,
+            price=update_product.price,
+            image_url=update_product.image_url,
+            stock=update_product.stock,
+            category_id=update_product.category))
+
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'Product update is successful'
+    }
 
 
 @router.delete('/delete')
-async def delete_product(product_id: int):
-    pass
+async def delete_product(db: Annotated[Session, Depends(get_db)], product_slug: str):
+    product = db.scalar(select(Product).where(Product.slug == product_slug))
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no category found'
+        )
+    db.execute(update(Product).where(Product.slug == product_slug).values(is_active=False))
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'Product delete is successful'
+    }
