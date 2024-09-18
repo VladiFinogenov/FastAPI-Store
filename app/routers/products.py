ee24c870bc57f6c68740f3984aab1c3ea833ff17
vlad <Finogenov.V.A@yandex.ru>
2024-09-18 12:44:45 +0300

    feat: доработаны эндпоинты для CRUD товаров

--- app/routers/products.py
+++ app/routers/products.py
@@ -16,20 +16,25 @@ router = APIRouter(prefix='/products', tags=['products'])
 
 @router.get('/')
 async def all_products(db: Annotated[Session, Depends(get_db)]):
-    products = db.scalars(select(Product).where(Product.is_active == True)).all()
+    products = db.scalars(select(Product).where(Product.is_active == True, Product.stock > 0)).all()
+    if not products:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There are no product'
+        )
     return products
 
 
 @router.post('/create')
 async def create_product(db: Annotated[Session, Depends(get_db)], create_product: CreateProduct):
     db.execute(insert(Product).values(name=create_product.name,
-                                      slug=create_slug(create_product.name),
                                       description=create_product.description,
                                       price=create_product.price,
                                       image_url=create_product.image_url,
                                       stock=create_product.stock,
-                                      category_id= create_product.category,
-                                      rating=0.0))
+                                      category_id=create_product.category,
+                                      rating=0.0,
+                                      slug=create_slug(create_product.name)))
     db.commit()
     return {
         'status_code': status.HTTP_201_CREATED,
@@ -38,20 +43,42 @@ async def create_product(db: Annotated[Session, Depends(get_db)], create_product
 
 
 @router.get('/{category_slug}')
-async def product_by_category(category_slug: str):
-    pass
+async def product_by_category(db: Annotated[Session, Depends(get_db)], category_slug: str):
+    category = db.scalar(select(Category).where(Category.slug == category_slug))
 
+    if not category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Category not found'
+        )
+
+    subcategories = db.scalars(select(Category).where(Category.parent_id == category.id)).all()
+
+    category_ids = [category.id] + [subcategory.id for subcategory in subcategories]
+
+    products_category = db.scalars(
+        select(Product).where(
+            Product.category_id.in_(category_ids),
+            Product.is_active == True,
+            Product.stock > 0
+        )
+    ).all()
+
+    return products_category
 
 @router.get('/detail/{product_slug}')
 async def product_detail(db: Annotated[Session, Depends(get_db)], product_slug: str):
-    products = db.scalars(select(Product).where(Product.slug == product_slug))
+    product = db.scalar(
+        select(Product).where(
+            Product.slug == product_slug,
+            Product.is_active == True,
+            Product.stock > 0))
     if not product:
-        raise HTTPException(
+        return HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='There are no product'
         )
-
-    return products
+    return product
 
 
 @router.put('/detail/{product_slug}')
@@ -87,14 +114,14 @@ async def update_product(db: Annotated[Session, Depends(get_db)], product_slug: 
 
 
 @router.delete('/delete')
-async def delete_product(db: Annotated[Session, Depends(get_db)], product_slug: str):
-    product = db.scalar(select(Product).where(Product.slug == product_slug))
-    if not product:
+async def delete_product(db: Annotated[Session, Depends(get_db)], product_id: int):
+    product_delete = db.scalar(select(Product).where(Product.id == product_id))
+    if product_delete is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail='There is no category found'
+            detail='There is no product found'
         )
-    db.execute(update(Product).where(Product.slug == product_slug).values(is_active=False))
+    db.execute(update(Product).where(Product.id == product_id).values(is_active=False))
     db.commit()
     return {
         'status_code': status.HTTP_200_OK,
